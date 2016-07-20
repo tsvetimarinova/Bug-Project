4fed36f02aad0ce6741f5dbe1c134ac2b6a7d92c
PC <tsvetoslavka@abv.bg>
2016-07-20 16:32:44 +0300

    hhgfd

--- querytest2.php
+++ querytest2.php
@@ -25,14 +25,14 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
     <body>
             <?php
 
-$query = "SELECT * FROM users";
+$query = "SELECT * FROM bugs";
 // execute the SQL query
 $result = mysql_query($query);
 if(!$result) die ("Could not query: " . mysql_error());
 $rows = mysql_num_rows($result);
 for($j = 0; $j < $rows; ++$j)
 {
-    echo 'userID: ' . mysql_result($result, $j, 'userID') . '</br>';
+    echo 'bug_name: ' . mysql_result($result, $j, 'bug_name') . '</br>';
 
 }
 // iterate over $result object one $row at a time
