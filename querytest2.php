e409581bbe1883c37d86046b7a594ed131fc55dc
PC <tsvetoslavka@abv.bg>
2016-07-20 16:30:19 +0300

    cndsjkc

--- querytest2.php
+++ querytest2.php
@@ -25,14 +25,14 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
     <body>
             <?php
 
-$query = "SELECT * FROM users";
+$query = "SELECT * FROM tags";
 // execute the SQL query
 $result = mysql_query($query);
 if(!$result) die ("Could not query: " . mysql_error());
 $rows = mysql_num_rows($result);
 for($j = 0; $j < $rows; ++$j)
 {
-    echo 'userID: ' . mysql_result($result, $j, 'userID') . '</br>';
+    echo 'tagID: ' . mysql_result($result, $j, 'tagID') . '</br>';
 
 }
 // iterate over $result object one $row at a time
